be081a08d6c420cc1f07e17a43b73f4d81db84df
nankaliali <ali.dada.28880@gmail.com>
2024-09-10 10:19:16 +0000

    first commit

---
+++ ProductTrendsAnalysis/get_products_of_almarsa.py
@@ -0,0 +1,67 @@
+from playwright.sync_api import sync_playwright
+
+
+def get_product_elements(page):
+    return page.query_selector_all("ul.S4WbK_.uQ5Uah.c2Zj9x.H1ux6p > li")
+
+
+def scrape_products():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(
+            headless=True
+        )  # Set headless=True for headless mode
+        page = browser.new_page()
+
+        with open("almarsa_category.txt", "r") as file:
+            links = [line.strip() for line in file.readlines()]
+
+        with open("product_urls_almarsa.txt", "a") as output_file:
+            for link in links:
+                page.goto(link)
+
+                all_products = set()
+
+                while True:
+                    # Scroll down incrementally
+                    page.evaluate("window.scrollBy(0, window.innerHeight)")
+                    page.wait_for_timeout(2000)  # Wait for 2 seconds
+
+                    # Get the new scroll height
+                    current_height = page.evaluate(
+                        "window.scrollY + window.innerHeight"
+                    )
+                    total_height = page.evaluate(
+                        "document.documentElement.scrollHeight"
+                    )
+
+                    # Check if we have reached the bottom of the page
+                    if current_height + 10 >= total_height:
+                        # Scroll up by 500 pixels before breaking
+                        page.evaluate("window.scrollBy(0, -500)")
+                        page.wait_for_timeout(
+                            2000
+                        )  # Wait for 2 seconds after scrolling up
+                        break
+
+                # Get product elements after scrolling
+                products = get_product_elements(page)
+                for product in products:
+                    a_tag_of_product = product.query_selector(" div > div > a")
+                    product_url = a_tag_of_product.get_attribute("href")
+                    if product_url:
+                        all_products.add(product_url)
+                    else:
+                        print("asd")
+
+                print(f"Total products found: {len(all_products)}")
+
+                # Write all product URLs to the output file
+                for product_url in all_products:
+                    output_file.write(f"{product_url}\n")
+                    print(product_url)
+
+        browser.close()
+
+
+if __name__ == "__main__":
+    scrape_products()
